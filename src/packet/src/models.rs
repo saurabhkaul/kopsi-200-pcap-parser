1c3af50a2a7802a9e048c7c6ef445e137c2ebc23
saurabh kaul <saurabh.rockstar90@gmail.com>
2025-07-29 02:17:29 +0530

    saving inbetween figuring out parsing regression

--- src/packet/src/models.rs
+++ src/packet/src/models.rs
@@ -87,99 +87,181 @@ impl <'a,const N:usize> PartialOrd<Self> for QuotePacket<'a,N> {
 
 
 
-fn parse_str_in_packet(slice: &[u8]) -> Result<&str, PcapError<&[u8]>> {
+fn parse_ascii_str_in_packet(slice: &[u8]) -> Result<&str, PcapError<&[u8]>> {
     str::from_utf8(slice).map_err(|_e| {
         PcapError::NomError(slice, ErrorKind::Verify)
     })
+    // if slice.iter().all(|&b| b.is_ascii()) {
+    //        Ok(unsafe { str::from_utf8_unchecked(slice) })
+    //    } else {
+    //        Err(PcapError::NomError(slice, ErrorKind::Verify))
+    //    }
+    // Not checking for ascii or utf8 since this is in the hot path
+    // Ok(unsafe { str::from_utf8_unchecked(slice)})
 }
 
 impl <'a,const N:usize>TryFrom<&PacketDataWithTime<'a>> for QuotePacket<'a,N> {
     type Error = PcapError<&'a [u8]>;
     
-    fn try_from(data:&PacketDataWithTime<'a>) -> Result<Self, Self::Error> {
-        let (packet_time,data,ordering) = (data.packet_timestamp,&data.data,data.ordering);
-        let data_slice = match data {
-            PacketData::L2(d) => d,
-            _ => return Err(PcapError::HeaderNotRecognized)
-        };
-        let mut offset = 42; //Packet data starts from the 42nd byte.
-
-        let data_type = parse_str_in_packet(&data_slice[offset..offset+2])?;
-        offset += 2;
-
-        let information_type = parse_str_in_packet(&data_slice[offset..offset+2])?;
-        offset += 2;
-
-        let market_type = data_slice[offset] as char;
-        offset += 1;
-
-        let packet_identifier = format!("{}{}{}",data_type,information_type,market_type);
-
-        if packet_identifier != PACKET_IDENTIFIER {
-             return Err(Self::Error::from_data(
-                 &data_slice[..offset],
-                 ErrorKind::IsNot
-             ))
-        }else {
-            let issue_code = parse_str_in_packet(&data_slice[offset..offset+12])?;
+    // fn try_from(data:&PacketDataWithTime<'a>) -> Result<Self, Self::Error> {
+       
+    //     let (packet_time,data,ordering) = (data.packet_timestamp,&data.data,data.ordering);
+    //             let data_slice = match data {
+    //                 PacketData::L2(d) => d,
+    //                 _ => return Err(PcapError::HeaderNotRecognized)
+    //             };
+    //             let mut offset = 42;
         
-            offset += 24;
+    //             if data_slice.len() < offset + 5 {
+    //                 return Err(PcapError::NomError(&data_slice[offset..], ErrorKind::Eof));
+    //             }
+                
+    //             if &data_slice[offset..offset+5] != b"B6034" {
+    //                 return Err(PcapError::NomError(&data_slice[offset..offset+5], ErrorKind::IsNot));
+    //             }
+    //             offset += 5;
+        
+    //             let issue_code = parse_ascii_str_in_packet(&data_slice[offset..offset+12])?;
+    //             offset += 24;
+        
+    //             let mut bid_prices = ["";N];
+    //             let mut bid_quantities = ["";N];
+    //             let mut ask_prices = ["";N];
+    //             let mut ask_quantities = ["";N];
+        
+    //             // Batch boundary check to avoid repeated bounds checking
+    //             let required_len = offset + (5 * (5 + 7)) + 7 + (5 * (5 + 7)) + 50 + 8;
+    //             if data_slice.len() < required_len {
+    //                 return Err(PcapError::NomError(&data_slice[offset..], ErrorKind::Eof));
+    //             }
+        
+    //             for i in 0..5 {
+    //                 bid_prices[i] = parse_ascii_str_in_packet(&data_slice[offset..offset + 5])?;
+    //                 offset += 5;
+    //                 bid_quantities[i] = parse_ascii_str_in_packet(&data_slice[offset..offset + 7])?;
+    //                 offset += 7;
+    //             }
+                
+    //             offset += 7;
+        
+    //             for i in 0..5 {
+    //                 ask_prices[i] = parse_ascii_str_in_packet(&data_slice[offset..offset + 5])?;
+    //                 offset += 5;
+    //                 ask_quantities[i] = parse_ascii_str_in_packet(&data_slice[offset..offset + 7])?;
+    //                 offset += 7;
+    //             }
+        
+    //             offset += 50;
+        
+    //             // Direct byte parsing for time
+    //             let time_bytes = &data_slice[offset..offset+8];
+    //             if !time_bytes.iter().all(|&b| b.is_ascii_digit()) {
+    //                 return Err(PcapError::NomError(time_bytes, ErrorKind::Verify));
+    //             }
+        
+    //             let hours = (time_bytes[0] - b'0') as u32 * 10 + (time_bytes[1] - b'0') as u32;
+    //             let minutes = (time_bytes[2] - b'0') as u32 * 10 + (time_bytes[3] - b'0') as u32;
+    //             let seconds = (time_bytes[4] - b'0') as u32 * 10 + (time_bytes[5] - b'0') as u32;
+    //             let centisecs = (time_bytes[6] - b'0') as u32 * 10 + (time_bytes[7] - b'0') as u32;
+    //             let microseconds = centisecs * 10000;
+        
+    //             let quote_accept_time = NaiveTime::from_hms_micro_opt(hours, minutes, seconds, microseconds)
+    //                 .ok_or(PcapError::NomError(time_bytes, ErrorKind::Verify))?;
+        
+    //             Ok(QuotePacket {
+    //                 packet_time,
+    //                 issue_code,
+    //                 bid_prices,
+    //                 bid_quantities,
+    //                 ask_prices,
+    //                 ask_quantities,
+    //                 quote_accept_time,
+    //                 ordering,
+    //             })
+
+    // }
+    fn try_from(data:&PacketDataWithTime<'a>) -> Result<Self, Self::Error> {
+            let (packet_time,data,ordering) = (data.packet_timestamp,&data.data,data.ordering);
+            let data_slice = match data {
+                PacketData::L2(d) => d,
+                _ => return Err(PcapError::HeaderNotRecognized)
+            };
+            let mut offset = 42; //Packet data starts from the 42nd byte.
+    
+            let data_type = parse_ascii_str_in_packet(&data_slice[offset..offset+2])?;
+            offset += 2;
+    
+            let information_type = parse_ascii_str_in_packet(&data_slice[offset..offset+2])?;
+            offset += 2;
+    
+            let market_type = data_slice[offset] as char;
+            offset += 1;
+    
+            let packet_identifier = format!("{}{}{}",data_type,information_type,market_type);
+    
+            if packet_identifier != PACKET_IDENTIFIER {
+                 return Err(Self::Error::from_data(
+                     &data_slice[..offset],
+                     ErrorKind::IsNot
+                 ))
+            }else {
+                let issue_code = parse_ascii_str_in_packet(&data_slice[offset..offset+12])?;
             
-
-            let mut bid_prices = ["";N];
-            let mut bid_quantities = ["";N];
-            let mut ask_prices = ["";N];
-            let mut ask_quantities = ["";N];
-
-            for i in 0..5 {
-
-                // bid_prices.push(parse_str_in_packet(&data_slice[offset..offset + 5])?);
-                bid_prices[i] = parse_str_in_packet(&data_slice[offset..offset + 5])?;
-                offset += 5;
-
-                // bid_quantities.push(parse_str_in_packet(&data_slice[offset..offset + 5])?);
-                bid_quantities[i] = parse_str_in_packet(&data_slice[offset..offset + 7])?;
+                offset += 24;
+                
+    
+                let mut bid_prices = ["";N];
+                let mut bid_quantities = ["";N];
+                let mut ask_prices = ["";N];
+                let mut ask_quantities = ["";N];
+    
+                for i in 0..5 {
+    
+                    // bid_prices.push(parse_ascii_str_in_packet(&data_slice[offset..offset + 5])?);
+                    bid_prices[i] = parse_ascii_str_in_packet(&data_slice[offset..offset + 5])?;
+                    offset += 5;
+    
+                    // bid_quantities.push(parse_ascii_str_in_packet(&data_slice[offset..offset + 5])?);
+                    bid_quantities[i] = parse_ascii_str_in_packet(&data_slice[offset..offset + 7])?;
+                    offset += 7;
+                }
+                
                 offset += 7;
+    
+                for i in 0..5 {
+                    // ask_prices.push(parse_ascii_str_in_packet(&data_slice[offset..offset+5])?.to_string());
+                    ask_prices[i] = parse_ascii_str_in_packet(&data_slice[offset..offset + 5])?;
+                    offset += 5;
+                    // ask_quantities.push(parse_ascii_str_in_packet(&data_slice[offset..offset+7])?.to_string());
+                    ask_quantities[i] = parse_ascii_str_in_packet(&data_slice[offset..offset + 7])?;
+                    offset += 7;
+                }
+    
+                offset += 50;
+    
+                
+                let quote_accept_time_string = parse_ascii_str_in_packet(&data_slice[offset..offset+8])?.to_string();
+    
+                let hours = u32::from_str_radix(&quote_accept_time_string[0..2], 10).unwrap();
+                let minutes = u32::from_str_radix(&quote_accept_time_string[2..4], 10).unwrap();
+                let seconds = u32::from_str_radix(&quote_accept_time_string[4..6], 10).unwrap();
+                let centisecs = u32::from_str_radix(&quote_accept_time_string[6..8], 10).unwrap();
+                let microseconds = centisecs * 10000;
+    
+                let quote_accept_time = NaiveTime::from_hms_micro_opt(hours, minutes, seconds, microseconds).unwrap();
+    
+                Ok(QuotePacket {
+                    packet_time,
+                    issue_code,
+                    bid_prices,
+                    bid_quantities,
+                    ask_prices,
+                    ask_quantities,
+                    quote_accept_time,
+                    ordering,
+                })
             }
-            
-            offset += 7;
-
-            for i in 0..5 {
-                // ask_prices.push(parse_str_in_packet(&data_slice[offset..offset+5])?.to_string());
-                ask_prices[i] = parse_str_in_packet(&data_slice[offset..offset + 5])?;
-                offset += 5;
-                // ask_quantities.push(parse_str_in_packet(&data_slice[offset..offset+7])?.to_string());
-                ask_quantities[i] = parse_str_in_packet(&data_slice[offset..offset + 7])?;
-                offset += 7;
-            }
-
-            offset += 50;
-
-            
-            let quote_accept_time_string = parse_str_in_packet(&data_slice[offset..offset+8])?.to_string();
-
-            let hours = u32::from_str_radix(&quote_accept_time_string[0..2], 10).unwrap();
-            let minutes = u32::from_str_radix(&quote_accept_time_string[2..4], 10).unwrap();
-            let seconds = u32::from_str_radix(&quote_accept_time_string[4..6], 10).unwrap();
-            let centisecs = u32::from_str_radix(&quote_accept_time_string[6..8], 10).unwrap();
-            let microseconds = centisecs * 10000;
-
-            let quote_accept_time = NaiveTime::from_hms_micro_opt(hours, minutes, seconds, microseconds).unwrap();
-
-            Ok(QuotePacket {
-                packet_time,
-                issue_code,
-                bid_prices,
-                bid_quantities,
-                ask_prices,
-                ask_quantities,
-                quote_accept_time,
-                ordering,
-            })
         }
-
-
-    }
 }
 
 
